1b46fd98ede1c7a1dd0ac07371a70052d63690e0
Alessandro <candido.ale@gmail.com>
2019-01-15 20:38:49 +0100

    Added basic classes for triangulation definition

---
+++ label.h
@@ -0,0 +1,71 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2019  Alessandro Candido <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LABEL_H
+#define LABEL_H
+
+/**
+ * @todo write docs
+ */
+class Label
+{
+public:
+    /**
+     * Default constructor
+     */
+    Label();
+
+    /**
+     * Copy constructor
+     *
+     * @param other TODO
+     */
+    Label ( const Label& other );
+
+    /**
+     * Destructor
+     */
+    ~Label();
+
+    /**
+     * Assignment operator
+     *
+     * @param other TODO
+     * @return TODO
+     */
+    Label& operator= ( const Label& other );
+
+    /**
+     * @todo write docs
+     *
+     * @param other TODO
+     * @return TODO
+     */
+    bool operator== ( const Label& other ) const;
+
+    /**
+     * @todo write docs
+     *
+     * @param other TODO
+     * @return TODO
+     */
+    bool operator!= ( const Label& other ) const;
+
+};
+
+#endif // LABEL_H
