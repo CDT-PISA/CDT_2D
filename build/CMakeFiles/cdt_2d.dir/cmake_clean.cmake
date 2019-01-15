90f81e4621ff2a96dcd07c8d7379090c01c585ce
Alessandro <candido.ale@gmail.com>
2019-01-15 12:41:07 +0100

    second commit... I think...

---
+++ build/CMakeFiles/cdt_2d.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cdt_2d.dir/main.cpp.o"
+  "cdt_2d.pdb"
+  "cdt_2d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cdt_2d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
