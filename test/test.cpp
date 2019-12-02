4e5ccca5f5034c0cdca0d8cbfb58ad712dabfd3d
Federico Rottoli <federottoli95@gmail.com>
2019-12-02 13:10:40 +0100

    Adapted test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -23,8 +23,8 @@ int dice()
     static RandomGen r;
     int dice = 0;
     
-    double move22 = 0.1;
-    double move24 = 0.2;
+    double move22 = 0.2;
+    double move24 = 0.3;
     
     double extraction = r.next();
     if(extraction < move22)
@@ -44,7 +44,11 @@ int dice()
 int main(int argc, char* argv[]){
     Triangulation uni(3, 0.4, 6.);
     RandomGen r;
-    
+
+    ofstream fout;
+    fout.open("test.dat", ios::out);
+
+ /*   
     for(int i=0; i<1000; i++){
         uni.move_24();
         x = uni.move_22();
@@ -79,7 +83,7 @@ int main(int argc, char* argv[]){
             cout << current - previous << endl;
             throw runtime_error("not gauge invariant");
         }
-    }
+    }*/
 //     complex<double> prev = exp(1i*4.6);
 //     cout << (imag(prev) > 0) * 2 - 1 << endl;
 //     
@@ -88,46 +92,61 @@ int main(int argc, char* argv[]){
 //     
 //     cout << U << endl << V << endl << 2 * V << endl;
     
+    double top;
+    double avgtop = 0, avgtop2 = 0, vartop = 0;
 
-//     for(int i=0; i<1000; ++i){
-// 
-//         switch(dice()){
-//             case 1:
-//             {
-// //                cout<<"mv22"<<endl;
-//                 uni.move_22();
-//                 break;
-//             }
-//             case 2:
-//             {
-// //                cout<<"mv22"<<endl;
-//                 uni.move_22();
-//                 break;
-//             }
-//             case 3:
-//             {
-// //                cout<<"mv24"<<endl;
-//                 uni.move_24();
-//                 break;   
-//             }
-//             case 4:
-//             {
-// //                cout<<"mv42"<<endl;
-//                 uni.move_42();
-//                 break;
-//             }
-//             case 5:
-//             {
-//                 uni.move_gauge();
-//                 break;
-//             }
-//         }
-//         cout<<i<<" "<<uni.topological_charge()<<endl;     
-//        for(int s = 0; s < 10; ++s){
-// //        cout<<10*i+s<<" "<<uni.topological_charge()<<endl;     
-//            uni.move_gauge();
-//        }
-//     }
+    int i;
+     for(i=0; i<1000; ++i){
+ 
+         switch(dice()){
+             case 1:
+             {
+ //                cout<<"mv22"<<endl;
+                 uni.move_22();
+                 break;
+             }
+             case 2:
+             {
+ //                cout<<"mv22"<<endl;
+                 uni.move_22();
+                 break;
+             }
+             case 3:
+             {
+ //                cout<<"mv24"<<endl;
+                 uni.move_24();
+                 break;   
+             }
+             case 4:
+             {
+ //                cout<<"mv42"<<endl;
+                 uni.move_42();
+                 break;
+             }
+             case 5:
+             {
+                 uni.move_gauge();
+                 break;
+             }
+         }
 
+	top = uni.topological_charge();
+	avgtop += top;
+	avgtop2 += top*top;
+        cout<<i<<" "<<top<<endl; 	
+        for(int s = 0; s < 20; ++s){
+ //        cout<<10*i+s<<" "<<uni.topological_charge()<<endl;     
+            uni.move_gauge();
+        }
+	if(i%200==0){
+		
+	}
+     }
+
+     avgtop /= i;
+     avgtop2 /= i;
+     vartop = avgtop2 - (avgtop * avgtop);
+    cout << "avg top = " << avgtop << endl;
+    cout << "var top = " << vartop ;
     return 0;
 }
